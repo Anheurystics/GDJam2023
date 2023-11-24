f6a479867245ac703f004d18c661d614c5279cbd
Willard Torres <willard.torres1008@gmail.com>
2023-11-24 13:26:18 +0800

    Initial chase and attack animation for enemies

--- assets/scripts/enemy.gd
+++ assets/scripts/enemy.gd
@@ -1,6 +1,7 @@
 class_name Enemy extends CharacterBody3D
 
 const SPEED = 5.0
+const CHASE_SPEED = 8.0;
 const JUMP_VELOCITY = 4.5
 const EIGHTH = 1.0 / 8.0
 
@@ -23,25 +24,44 @@ var sprite_frames: SpriteFrames;
 @export var hurt_start_frame: String;
 @export var num_hurt_frames: int;
 
+@export var attack_start_frame: String;
+@export var num_attack_frames: int;
+
+var health: int = 50;
 var curr_anim_suffix: String = "";
+var angle_to_player: float = 0;
 
 var test_public: bool = false;
+var is_attacking: bool = false;
+var player_target: Player = null;
 
 func _ready():
 	var walk_frames = [];
 	for i in range(num_walk_frames):
 		walk_frames.push_back(char(walk_start_frame.to_ascii_buffer()[0] + i));
-	load_sprite_frames("walk", sprite_name, walk_frames);
+	load_sprite_frames("walk", sprite_name, walk_frames, true);
 	
 	var hurt_frames = [];
 	for i in range(num_hurt_frames):
 		hurt_frames.push_back(char(hurt_start_frame.to_ascii_buffer()[0] + i));
-	load_sprite_frames("hurt", sprite_name, hurt_frames);
+	load_sprite_frames("hurt", sprite_name, hurt_frames, false);
+	
+	var attack_frames = [];
+	for i in range(num_attack_frames):
+		attack_frames.push_back(char(attack_start_frame.to_ascii_buffer()[0] + i));
+	load_sprite_frames("attack", sprite_name, attack_frames, false);
+	
+	sprite.sprite_frames = sprite_frames;
+	sprite.animation_finished.connect(_on_sprite_animation_finished);
+	shadeSprite.sprite_frames = sprite_frames;
 	
 	await NavigationServer3D.map_changed;
 	nav_agent.link_reached.connect(_on_nav_agent_link_reached);
 	nav_agent.navigation_finished.connect(_on_nav_agent_navigation_finished);
 	
+func _on_sprite_animation_finished():
+	pass
+
 func falter_shade(duration: float):
 	set_shaded(false);
 	await get_tree().create_timer(duration).timeout;
@@ -56,22 +76,24 @@ func set_shaded(shaded: bool):
 		$Sprite.layers = 1;
 		$ShadeSprite.visible = false;
 
-func _on_nav_agent_link_reached(_details):
-	await get_tree().create_timer(0.5).timeout;
-	$Interactor.interact();
-	nav_paused = true;
-	await get_tree().create_timer(0.5).timeout;
-	nav_paused = false;
+func _on_nav_agent_link_reached(details):
+	var door: Door = details.owner.get_parent() as Door;
+	if door and not door.open:
+		await get_tree().create_timer(0.5).timeout;
+		$Interactor.interact();
+		nav_paused = true;
+		await get_tree().create_timer(0.5).timeout;
+		nav_paused = false;
 
 func _on_nav_agent_navigation_finished():
-	await get_tree().create_timer(2.0).timeout;
+	await get_tree().create_timer(1.0).timeout;
 	walk_random();
 
-func load_sprite_frames(anim_prefix: String, prefix: String, frames: Array):
+func load_sprite_frames(anim_prefix: String, prefix: String, frames: Array, loop: bool):
 	if frames.is_empty():
 		return;
 
-	var indices = [["1"], ["2", "8"], ["3", "7"], ["4", "6"], ["5"]] if sprite_mirrored  else [["1"], ["2"], ["3"], ["4"], ["5"], ["6"], ["7"], ["8"]];
+	var indices = [["1"], ["2", "8"], ["3", "7"], ["4", "6"], ["5"]] if sprite_mirrored else [["1"], ["2"], ["3"], ["4"], ["5"], ["6"], ["7"], ["8"]];
 	
 	if not sprite_frames:
 		sprite_frames = SpriteFrames.new();
@@ -80,6 +102,7 @@ func load_sprite_frames(anim_prefix: String, prefix: String, frames: Array):
 		var arr: PackedStringArray = (i);
 		var anim_name = anim_prefix + "_" + "_".join(arr);
 		sprite_frames.add_animation(anim_name);
+		sprite_frames.set_animation_loop(anim_name, loop);
 		for f in frames:
 			# eg. a1, b2b8, etc.
 			var suffix = "";
@@ -88,8 +111,6 @@ func load_sprite_frames(anim_prefix: String, prefix: String, frames: Array):
 				
 			var path = "res://assets/freedoom/sprites/%s%s.png" % [prefix, suffix];
 			sprite_frames.add_frame(anim_name, load(path));
-	sprite.sprite_frames = sprite_frames;
-	shadeSprite.sprite_frames = sprite_frames;
 
 func walk_random():
 	var random = Vector3(randf_range(-20.0, 20.0), 0, randf_range(-20.0, 20.0));
@@ -99,7 +120,7 @@ func walk_random():
 func handle_flash(source_position: Vector3):
 	var angle = get_face_angle_to_position(source_position);
 	var ratio = abs(angle) / PI;
-	print(ratio);
+	# print(ratio);
 	# if ratio > 0.75:
 	#	set_shaded(false);
 	#	deal_damage();
@@ -108,10 +129,7 @@ func handle_flash(source_position: Vector3):
 	
 func deal_damage():
 	nav_paused = true;
-	if sprite.sprite_frames.has_animation("hurt_" + curr_anim_suffix):
-		sprite.play("hurt_" + curr_anim_suffix);
-	if shadeSprite.sprite_frames.has_animation("hurt_" + curr_anim_suffix):
-		shadeSprite.play("hurt_" + curr_anim_suffix);
+	play_animation("hurt");
 
 func choose_flipped_sprite_suffix(flip: bool, a: String, b: String):
 	if sprite_mirrored:
@@ -137,9 +155,9 @@ func get_face_angle_to_position(pos: Vector3):
 	return angle;
 
 func _process(_delta):
-	var angle = get_face_angle_to_position(get_viewport().get_camera_3d().global_position);
-	var pi_ratio = abs(angle) / PI;
-	var flip = angle < 0;
+	angle_to_player = get_face_angle_to_position(get_viewport().get_camera_3d().global_position);
+	var pi_ratio = abs(angle_to_player) / PI;
+	var flip = angle_to_player < 0;
 
 	if pi_ratio < EIGHTH:
 		curr_anim_suffix = "5";
@@ -152,16 +170,15 @@ func _process(_delta):
 	else:
 		curr_anim_suffix = "1";
 
-	sprite.play("walk_" + curr_anim_suffix);
-	shadeSprite.play("walk_" + curr_anim_suffix);
+	if not is_attacking:
+		play_animation("walk");
 	
-	if velocity.length_squared() > 0.1:
-		sprite.speed_scale = 1;
-		shadeSprite.speed_scale = 1;
-	else:
-		sprite.speed_scale = 0;
+	var curr_speed = velocity.length();
+	var speed_scale = curr_speed / SPEED;
+	sprite.speed_scale = speed_scale;
+	shadeSprite.speed_scale = speed_scale;
+	if curr_speed == 0.0:
 		sprite.set_frame_and_progress(1, 0);
-		shadeSprite.speed_scale = 0;
 		shadeSprite.set_frame_and_progress(1, 0);
 
 func _physics_process(delta):
@@ -172,16 +189,47 @@ func _physics_process(delta):
 	velocity = Vector3.ZERO;
 	if not nav_agent.is_navigation_finished():
 		var target = nav_agent.get_next_path_position();
-		if global_position.distance_to(target) > 0.1 and not nav_paused:
+		if global_position.distance_to(target) > 0.1 and not nav_paused and not is_attacking:
 			var look_at_target = Vector3(target);
 			look_at_target.y = global_position.y;
 			look_at(look_at_target);
 			var to_target =  target - global_position;
 			to_target.y = 0;
-			velocity = to_target.normalized() * SPEED;
-
+			velocity = to_target.normalized() * (CHASE_SPEED if player_target else SPEED);
+	
+	check_player_target();
 	move_and_slide()
 
+func check_player_target():
+	if abs(angle_to_player) > PI * 6 * EIGHTH:
+		var space_state = get_parent().get_world_3d().direct_space_state;
+		var player_ray_query = PhysicsRayQueryParameters3D.create(global_position, get_viewport().get_camera_3d().global_position);
+		player_ray_query.collide_with_areas = false;
+		player_ray_query.collide_with_bodies = true;
+		player_ray_query.collision_mask = ~2;
+		
+		var player_result = space_state.intersect_ray(player_ray_query);
+		var collider = player_result.collider;
+		if collider and collider is Player:
+			print("found player");
+			player_target = collider as Player;
+
 func navigate_to(target: Vector3):
+	nav_agent.target_desired_distance = 2.0;
 	nav_agent.target_position = target;
 
+func play_animation(prefix: String):
+	var full_anim_name = prefix + "_" + curr_anim_suffix;
+	if sprite.sprite_frames.has_animation(full_anim_name):
+		sprite.play(full_anim_name);
+	if shadeSprite.sprite_frames.has_animation(full_anim_name):
+		shadeSprite.play(full_anim_name);
+
+func attack(player: Player):
+	is_attacking = true;
+	play_animation("attack");
+	await get_tree().create_timer(1.0).timeout;
+	player.modify_health(-10);
+	play_animation("walk");
+	await get_tree().create_timer(1.0).timeout;
+	is_attacking = false;
--- assets/scripts/shade.gd
+++ assets/scripts/shade.gd
@@ -1,6 +1,32 @@
 class_name Shade extends Enemy
 
+var is_following_player: bool = false;
+
+var attack_interval;
+
 func _ready():
 	super._ready();
-	set_shaded(true);
+	set_shaded(false);
 	walk_random();
+
+func _process(delta):
+	super._process(delta);
+	if player_target and not is_following_player:
+		is_following_player = true;
+		navigate_to(player_target.global_position);
+		
+	if is_following_player:
+		if not player_target:
+			is_following_player = false;
+		else:
+			var diff = player_target.global_position - global_position;
+			diff.y = 0;
+			if diff.length() < 2.0 and not is_attacking:
+				attack(player_target);
+
+func _on_nav_agent_navigation_finished():
+	if is_following_player:
+		navigate_to(player_target.global_position);
+	else:
+		super._on_nav_agent_navigation_finished()
+	
