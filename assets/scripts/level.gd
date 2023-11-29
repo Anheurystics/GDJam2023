b6b70fcaf9ad296607a0e5c84a8c361a32f32713
Willard Torres <willard.torres1008@gmail.com>
2023-11-29 18:26:28 +0800

    Modify how game loading works to remove deleted nodes

--- assets/scripts/level.gd
+++ assets/scripts/level.gd
@@ -2,8 +2,11 @@ extends Node3D
 
 func _ready():
 	if Progress.quick_load_cache:
-		for node_path in Progress.quick_load_cache:
-			var node = get_node(node_path);
-			if node and node.has_method("deserialize"):
-				node.deserialize(Progress.quick_load_cache[node_path]);
+		for node in get_tree().get_nodes_in_group("persist"):
+			var path = node.get_path();
+			if Progress.quick_load_cache.has(path):
+				if node.has_method("deserialize"):
+					node.deserialize(Progress.quick_load_cache[path]);
+			else:
+				node.queue_free();
 		Progress.quick_load_cache = {};
